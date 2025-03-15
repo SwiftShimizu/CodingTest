51d67b8f2dba0277a7361caae3fa01f4daca1cfa
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-03-16 08:34:54 +0900

    Initial Commit

---
+++ CodingTest/CodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTestApp.swift
+//  CodingTest
+//
+//  Created by 志水拓哉 on 2025/03/16.
+//
+
+import SwiftUI
+
+@main
+struct CodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
